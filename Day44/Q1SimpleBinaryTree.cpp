896782beb17db288d8d74633aa97b3f6fa32dc9b
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2024-10-27 21:59:29 +0530

    Update Q1SimpleBinaryTree.cpp

--- Day44/Q1SimpleBinaryTree.cpp
+++ Day44/Q1SimpleBinaryTree.cpp
@@ -1,1 +1,32 @@
-hey
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+int main() {
+    Node* root = new Node(1); 
+    root->left = new Node(2); 
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Root Node: " << root->data << endl;
+    cout << "Left child of root: " << root->left->data << endl;
+    cout << "Right child of root: " << root->right->data << endl;
+    cout << "Left child of node 2: " << root->left->left->data << endl;
+    cout << "Right child of node 2: " << root->left->right->data << endl;
+
+    delete root->left->left;
+    delete root->left->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+
+    return 0;
+}
