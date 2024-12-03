a4753b19d61fe8f9cad14635e8fa79b1cefe0c17
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2024-12-03 23:24:55 +0530

    Create Q1AVLTree.cpp

---
+++ Day55/Q1AVLTree.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(int val) : data(val), left(nullptr), right(nullptr), height(1) {}
+};
+
+int height(Node* node) {
+    if (!node) return 0;
+    return node->height;
+}
+
+int balanceFactor(Node* node) {
+    if (!node) return 0;
+    return height(node->left) - height(node->right);
+}
+
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+Node* insert(Node* node, int key) {
+    if (!node) return new Node(key);
+
+    if (key < node->data)
+        node->left = insert(node->left, key);
+    else if (key > node->data)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    node->height = max(height(node->left), height(node->right)) + 1;
+
+    int balance = balanceFactor(node);
+
+    if (balance > 1 && key < node->left->data)
+        return rightRotate(node);
+
+    if (balance < -1 && key > node->right->data)
+        return leftRotate(node);
+
+    if (balance > 1 && key > node->left->data) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && key < node->right->data) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void preOrder(Node* root) {
+    if (root) {
+        cout << "Data: " << root->data 
+             << " Height: " << root->height
+             << " Balance Factor: " << balanceFactor(root) << endl;
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int computeTreeHeight(Node* root) {
+    if (!root) return 0;
+    return root->height;
+}
+
+int main() {
+    Node* root = nullptr;
+
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 15);
+    root = insert(root, 25);
+
+    cout << "Pre-order traversal with height and balance factor:\n";
+    preOrder(root);
+
+    cout << "\nHeight of the tree: " << computeTreeHeight(root) << endl;
+
+    return 0;
+}
